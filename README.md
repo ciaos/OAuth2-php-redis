0d76502f99a2190efeb841d999039d62768d0dba
ciaos <pengjing87@gmail.com>
2012-11-13 09:25:36 +0800

    1.0 version
    server side driver of redis for OAuth2

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿OAuth2 PHP Redis Driver
+=====================
+基于Predis客户端做的OAuth2.0服务器端数据库驱动
+* * *
+
+Description
+-----------
+
+[OAuth2-php](http://code.google.com/p/oauth2-php/)
+[Predis](https://github.com/nrk/predis)
+
+Weibo Account
+-------------
+
+Have a question? [@littley](http://weibo.com/littley)
+
---
+++ server/examples/redis/token.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Sample token endpoint.
+ *
+ * Obviously not production-ready code, just simple and to the point.
+ *
+ * In reality, you'd probably use a nifty framework to handle most of the crud for you.
+ */
+
+require "lib/RedisOAuth2.inc";
+
+$oauth = new RedisOAuth2();
+
+$oauth->grantAccessToken();
